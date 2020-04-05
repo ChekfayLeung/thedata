629748bb976d94600da9fb5ba7b41193bfc1a8e7
leung <leung@leung.y430p>
2020-04-05 18:28:12 +0800

    move pandas function to numpy #numba required

--- merge.py
+++ merge.py
@@ -1,4 +1,8 @@
 """中心检查数据库用"""
+import pandas as pd, tkinter.filedialog as tkf, sys, os, re, numpy as np, tkinter as tk, multiprocessing
+from it.data_clean import *
+from it import *;from multiprocessing import Pool
+
 def afile():
     f = open("{}\\{}log.txt".format(dire,re.split("[\\\/\.]",filepath)[-2]), "a+")
     f.write("{}\n".format(filepath))
@@ -9,159 +13,156 @@ def afile():
     f.write("----------------------------------\n\n")
     f.close()
 
+def read_file(filepath):
+    cwd = os.getcwd()
+    data = pd.DataFrame(columns=
+                        ['id', 'age', 'sex', "birthday", 'inday', 'outday', 'stayday', 'dept1', 'dept2',
+                         'disease',
+                         'icd10',
+                         'disease_2', 'icd10_2',
+                         'disease_3', 'icd10_3', 'disease_4', 'icd10_4', 'totalexp', 'treatexp', 'drug',
+                         'drug_w',
+                         'drug_chn1', 'drug_chn2',
+                         'bedfee', 'reg', 'consultation', 'diagnose', 'check', 'surgey', 'test', 'nurse',
+                         'other',
+                         'fs', 'fs1', 'fs2', 'reimburse', 'account', 'oop',
+                         'ms', 'hp_type', 'level', 'source', 'province', 'city', 'county', 'hp_name', 'hc',
+                         'pu-private',
+                         "CHECK", "from"])
+    i = 1
+    while True:
+        try:
+            print("\r{}".format(i),end='')
+            cache = pd.read_excel(filepath, sheet_name=-i)
+            if "sex" in cache.columns:
+                data = pd.concat([data, cache], sort=False)
+            i += 1
+        except:
+            del cache
+            break
+    if True:
+        for bird in [x for x in data.columns if
+                     any([all([y in x for y in ['b', 'day']]), "出生日期" in x, "Birt" in x])]:
+            id = data['birthday'].isna()
+            data.loc[id, 'birthday'] = data.loc[id, bird]
+            if bird != "birthday": data.drop(bird, inplace=True, axis=1)
+        for stay in [x for x in data.columns if any([all([y in x for y in ['st', 'day']]), "实际住院天数" in x])]:
+            id = data['stayday'].isna()
+            data.loc[id, 'stayday'] = data.loc[id, stay]
+            if stay != "stayday": data.drop(stay, inplace=True, axis=1)
+        for age in [x for x in data.columns if any([all([y in x for y in ['age']]), "年龄" in x])]:
+            id = data['age'].isna()
+            data.loc[id, 'age'] = data.loc[id, age]
+            if age != "age": data.drop(age, inplace=True, axis=1)
+        for htype in [x for x in data.columns if any([all([y in x for y in ['h', "ame"]]), "医院名称" in x])]:
+            id = data['hp_name'].isna()
+            data.loc[id, 'hp_name'] = data.loc[id, htype]
+            if htype != "hp_name": data.drop(htype, inplace=True, axis=1)
+        for hc in [x for x in data.columns if all([all([y in x for y in ['h']]), len(x) <= 3])]:
+            id = data['hc'].isna()
+            data.loc[id, 'hc'] = data.loc[id, hc]
+            if hc != "hc": data.drop(hc, inplace=True, axis=1)
+    data['from'] = str(filepath)
+    data[["年龄", "性别", "出生日期"]] = data[['age', 'sex', 'birthday']].copy()
+    if "pu_private" in data.columns: data["pu-private"] = data['pu_private']
+    if "icd_10" in data.columns: data['icd10'] = data['icd-10']
+    if "surgry" in data.columns: data['surgey'] = data["surgry"]
+    if "durg" in data.columns: data["drug"] = data["durg"]
+    data["CHECK"] = ''
+    # if hos !='': data["hp_name"]=hos
+    for var in ["totalexp", 'treatexp', 'drug', 'drug_w', 'drug_chn1', 'drug_chn2',
+                'bedfee', 'reg', 'consultation', 'diagnose', 'check', 'surgey', 'test',
+                'nurse', 'other', 'reimburse', 'account', 'oop']:
+        data[var] = data[var].apply(standarizenum)
+    test1 = data[['totalexp', "reimburse", "oop", "account"]].fillna(0). \
+        eval("totalexp!=reimburse+account+oop")
+    test2 = data[["totalexp", 'treatexp', 'drug', 'drug_w', 'drug_chn1', 'drug_chn2',
+                  'bedfee', 'reg', 'consultation', 'diagnose', 'check', 'surgey', 'test',
+                  'nurse', 'other']].fillna(0). \
+        eval("totalexp!="
+             "treatexp+drug+bedfee+reg+consultation+"
+             "diagnose+check+surgey+test+nurse+other")
+    test = test1 + test2
+    data.loc[test, "CHECK"] += "费用错误;\n"
+    # data.loc[test,'totalexp'] = pd.DataFrame([
+    #     data[[ 'treatexp', 'drug','bedfee', 'reg', 'consultation', 'diagnose', 'check', 'surgey', 'test',
+    #              'nurse', 'other']].fillna(0).T.sum().values,
+    #     data[["reimburse","oop","account"]].fillna(0).T.sum().values]).T.max(axis=1)
+    for var in ['totalexp', 'drug', 'reimburse', "account", "oop"]:
+        data.loc[test, "CHECK"] += data.loc[test, var].apply(checkpositive, vars=var)
+    del test
+    # data=rename(data)
+    data.loc[data[['disease', "icd10"]].isna().T.apply(any), "CHECK"] += "疾病/ICD缺失;\n"
+    try:
+        get_age(data)
+    except:
+        pass
+    if True:  # 在这一步之前要将名字换回来 if necessary
+        data["CHECK"] += data[["fs1", "fs2"]].sum(axis=1).apply(checkpositive, vars="fs")
+        standarizegender(data)  # 整性别
+        for var in ['age', 'sex']:
+            data["CHECK"] += data[var].apply(checkpositive, vars=var)
+        data['CHECK'] += data['age'].apply(checktype, types=float, vars="age")  # 检查年龄
+        if len(pd.Categorical(data['sex']).categories) > 2:
+            fix = data.query("sex!='1' and sex !='2'")
+            pass  # 写按照比例填入性别的代码
+    # data = findloc(data)
+    data.sort_values(['CHECK'], ascending=False, na_position="last", inplace=True)
+    data = data[::-1].reset_index(drop=True)
+    if True:  # 保存步
+        print("进入保存阶段，请勿停止")
+        if any(data.eval("hc==2")):
+            data.loc[data.eval("hc==2"), ["CHECK", 'id', 'age', 'sex', 'birthday', 'disease', 'icd10', 'inday',
+                                          'dept1',
+                                          'totalexp',
+                                          'treatexp', 'drug', 'drug_w',
+                                          'drug_chn1', 'drug_chn2', 'reg', 'consultation', 'check', 'surgey',
+                                          'test',
+                                          'other', 'fs', "fs1", 'fs2', 'reimburse',
+                                          'account', 'oop', 'ms', 'hp_type', 'level', 'source', 'province',
+                                          'city',
+                                          'county', 'hp_name', 'hc',
+                                          'pu-private', 'from']]. \
+                to_csv(re.sub("\.xls\w?", "_门诊.csv", filepath), date_format='%Y/%m/%d', index=False)
+        if any(data.eval("hc==1")):
+            data.loc[data.eval("hc==1"),
+                     ["CHECK", 'id', 'age', 'sex', 'birthday', 'inday', 'outday', 'stayday', 'dept1', 'dept2',
+                      'disease',
+                      'icd10', 'disease_2', 'icd10_2',
+                      'disease_3', 'icd10_3', 'disease_4', 'icd10_4', 'totalexp', 'treatexp', 'drug', 'drug_w',
+                      'drug_chn1',
+                      'drug_chn2', 'bedfee', 'diagnose', 'check', 'surgey', 'test', 'other', 'fs', "fs1", 'fs2',
+                      'reimburse', 'account',
+                      'oop', 'ms', 'hp_type', 'level', 'source', 'province', 'city', 'county', 'hp_name', 'hc',
+                      'pu-private', 'from']]. \
+                to_csv(re.sub("\.xls\w?", "_住院.csv", filepath), date_format="%Y/%m/%d", index=False)
+        if any(data.eval("hc!=1 and hc!=2")):
+            data[data.eval("hc!=1 and hc!=2")].to_csv(re.sub("\.xls\w?", "_没hc.csv", filepath),
+                                                      date_format='%Y/%m/%d', index=False)
+        print("存在问题的数据:{}条".format(data.eval("CHECK!=''").count()))
+        dire = "\\".join(re.split("[\\/]", filepath)[:-1])
+        afile()
+        f = open("{}\\log.txt".format(cwd), 'a+')
+        f.write(open("{}\\{}log.txt""".format(dire, re.split("[\\\/\.]", filepath)[-2]), "r").read())
+        f.close()
+        del data
+
 def main():
     import pandas as pd, tkinter.filedialog as tkf, sys, os, re, numpy as np, tkinter as tk, multiprocessing
     from it.data_clean import *
-    from it import *
+    from it import *;
+    from multiprocessing import Pool;from tqdm import tqdm
     cwd = os.getcwd()
     f = open("{}\log.txt".format(cwd), 'w+')
     f.close()
     TK = tk.Tk()
     path = tkf.askdirectory()
     filepaths = getfile(path, keywords=input("识别字段？="))
-    # filepath = tkf.askopenfilenames() # Load Data
-    # if isinstance(filepath, tuple):
-    #     filepaths = list(filepath)
-    # else:
-    #     filepaths = [filepath[0]]
-    # filepaths=[x for x in filepaths if x.find(".xls")!=-1]
     TK.destroy()
     # province=input("province=")
     if True:
-        for filepath in filepaths:
-            # os.system("""start %s""" % filepath)
-            # hos=input("{}\nContinue? OR hp_name=".format(filepath))
-            data = pd.DataFrame(columns=
-                                ['id', 'age', 'sex', "birthday", 'inday', 'outday', 'stayday', 'dept1', 'dept2',
-                                 'disease',
-                                 'icd10',
-                                 'disease_2', 'icd10_2',
-                                 'disease_3', 'icd10_3', 'disease_4', 'icd10_4', 'totalexp', 'treatexp', 'drug',
-                                 'drug_w',
-                                 'drug_chn1', 'drug_chn2',
-                                 'bedfee', 'reg', 'consultation', 'diagnose', 'check', 'surgey', 'test', 'nurse',
-                                 'other',
-                                 'fs', 'fs1', 'fs2', 'reimburse', 'account', 'oop',
-                                 'ms', 'hp_type', 'level', 'source', 'province', 'city', 'county', 'hp_name', 'hc',
-                                 'pu-private',
-                                 "CHECK", "from"])
-            i = 1
-            while True:
-                try:
-                    print("\r{}".format(i))
-                    cache = pd.read_excel(filepath, sheet_name=-i)
-                    if "sex" in cache.columns:
-                        data = pd.concat([data, cache], sort=False)
-                    i += 1
-                except:
-                    del cache
-                    break
-            if True:
-                for bird in [x for x in data.columns if
-                             any([all([y in x for y in ['b', 'day']]), "出生日期" in x, "Birt" in x])]:
-                    id = data['birthday'].isna()
-                    data.loc[id, 'birthday'] = data.loc[id, bird]
-                    if bird != "birthday": data.drop(bird, inplace=True, axis=1)
-                for stay in [x for x in data.columns if any([all([y in x for y in ['st', 'day']]), "实际住院天数" in x])]:
-                    id = data['stayday'].isna()
-                    data.loc[id, 'stayday'] = data.loc[id, stay]
-                    if stay != "stayday": data.drop(stay, inplace=True, axis=1)
-                for age in [x for x in data.columns if any([all([y in x for y in ['age']]), "年龄" in x])]:
-                    id = data['age'].isna()
-                    data.loc[id, 'age'] = data.loc[id, age]
-                    if age != "age": data.drop(age, inplace=True, axis=1)
-                for htype in [x for x in data.columns if any([all([y in x for y in ['h', "ame"]]), "医院名称" in x])]:
-                    id = data['hp_name'].isna()
-                    data.loc[id, 'hp_name'] = data.loc[id, htype]
-                    if htype != "hp_name": data.drop(htype, inplace=True, axis=1)
-                for hc in [x for x in data.columns if all([all([y in x for y in ['h']]), len(x) <= 3])]:
-                    id = data['hc'].isna()
-                    data.loc[id, 'hc'] = data.loc[id, hc]
-                    if hc != "hc": data.drop(hc, inplace=True, axis=1)
-            data['from'] = str(filepath)
-            data[["年龄", "性别", "出生日期"]] = data[['age', 'sex', 'birthday']].copy()
-            if "pu_private" in data.columns: data["pu-private"] = data['pu_private']
-            if "icd_10" in data.columns: data['icd10'] = data['icd-10']
-            if "surgry" in data.columns: data['surgey'] = data["surgry"]
-            if "durg" in data.columns: data["drug"] = data["durg"]
-            data["CHECK"] = ''
-            # if hos !='': data["hp_name"]=hos
-            for var in ["totalexp", 'treatexp', 'drug', 'drug_w', 'drug_chn1', 'drug_chn2',
-                        'bedfee', 'reg', 'consultation', 'diagnose', 'check', 'surgey', 'test',
-                        'nurse', 'other', 'reimburse', 'account', 'oop']:
-                data[var] = data[var].apply(standarizenum)
-            test1 = data[['totalexp', "reimburse", "oop", "account"]].fillna(0). \
-                eval("totalexp!=reimburse+account+oop")
-            test2 = data[["totalexp", 'treatexp', 'drug', 'drug_w', 'drug_chn1', 'drug_chn2',
-                          'bedfee', 'reg', 'consultation', 'diagnose', 'check', 'surgey', 'test',
-                          'nurse', 'other']].fillna(0). \
-                eval("totalexp!="
-                     "treatexp+drug+bedfee+reg+consultation+"
-                     "diagnose+check+surgey+test+nurse+other")
-            test = test1 + test2
-            data.loc[test, "CHECK"] += "费用错误;\n"
-            # data.loc[test,'totalexp'] = pd.DataFrame([
-            #     data[[ 'treatexp', 'drug','bedfee', 'reg', 'consultation', 'diagnose', 'check', 'surgey', 'test',
-            #              'nurse', 'other']].fillna(0).T.sum().values,
-            #     data[["reimburse","oop","account"]].fillna(0).T.sum().values]).T.max(axis=1)
-            for var in ['totalexp', 'drug', 'reimburse', "account", "oop"]:
-                data.loc[test, "CHECK"] += data.loc[test, var].apply(checkpositive, vars=var)
-            del test
-            # data=rename(data)
-            data.loc[data[['disease', "icd10"]].isna().T.apply(any), "CHECK"] += "疾病/ICD缺失;\n"
-            try:
-                get_age(data)
-            except:
-                pass
-            if True:  # 在这一步之前要将名字换回来 if necessary
-                data["CHECK"] += data[["fs1", "fs2"]].sum(axis=1).apply(checkpositive, vars="fs")
-                standarizegender(data)  # 整性别
-                for var in ['age', 'sex']:
-                    data["CHECK"] += data[var].apply(checkpositive, vars=var)
-                data['CHECK'] += data['age'].apply(checktype, types=float, vars="age")  # 检查年龄
-                if len(pd.Categorical(data['sex']).categories) > 2:
-                    fix = data.query("sex!='1' and sex !='2'")
-                    pass  # 写按照比例填入性别的代码
-            # data = findloc(data)
-            data.sort_values(['CHECK'], ascending=False, na_position="last", inplace=True)
-            data = data[::-1].reset_index(drop=True)
-            if True:  # 保存步
-                print("进入保存阶段，请勿停止")
-                if any(data.eval("hc==2")):
-                    data.loc[data.eval("hc==2"), ["CHECK", 'id', 'age', 'sex', 'birthday', 'disease', 'icd10', 'inday',
-                                                  'dept1',
-                                                  'totalexp',
-                                                  'treatexp', 'drug', 'drug_w',
-                                                  'drug_chn1', 'drug_chn2', 'reg', 'consultation', 'check', 'surgey',
-                                                  'test',
-                                                  'other', 'fs', "fs1", 'fs2', 'reimburse',
-                                                  'account', 'oop', 'ms', 'hp_type', 'level', 'source', 'province',
-                                                  'city',
-                                                  'county', 'hp_name', 'hc',
-                                                  'pu-private', 'from']]. \
-                        to_csv(re.sub("\.xls\w?", "_门诊.csv", filepath), date_format='%Y/%m/%d', index=False)
-                if any(data.eval("hc==1")):
-                    data.loc[data.eval("hc==1"),
-                             ["CHECK", 'id', 'age', 'sex', 'birthday', 'inday', 'outday', 'stayday', 'dept1', 'dept2',
-                              'disease',
-                              'icd10', 'disease_2', 'icd10_2',
-                              'disease_3', 'icd10_3', 'disease_4', 'icd10_4', 'totalexp', 'treatexp', 'drug', 'drug_w',
-                              'drug_chn1',
-                              'drug_chn2', 'bedfee', 'diagnose', 'check', 'surgey', 'test', 'other', 'fs', "fs1", 'fs2',
-                              'reimburse', 'account',
-                              'oop', 'ms', 'hp_type', 'level', 'source', 'province', 'city', 'county', 'hp_name', 'hc',
-                              'pu-private', 'from']]. \
-                        to_csv(re.sub("\.xls\w?", "_住院.csv", filepath), date_format="%Y/%m/%d", index=False)
-                if any(data.eval("hc!=1 and hc!=2")):
-                    data[data.eval("hc!=1 and hc!=2")].to_csv(re.sub("\.xls\w?", "_没hc.csv", filepath),
-                                                              date_format='%Y/%m/%d', index=False)
-                print("存在问题的数据:{}条".format(data.eval("CHECK!=''").count()))
-                dire = "\\".join(re.split("[\\/]", filepath)[:-1])
-                afile()
-                f = open("{}\\log.txt".format(cwd), 'a+')
-                f.write(open("{}\\{}log.txt""".format(dire, re.split("[\\\/\.]", filepath)[-2]), "r").read())
-                f.close()
-                del data
+        with Pool(4) as p:
+            p.map(read_file,filepaths)
     if True:
         data = pd.DataFrame(columns=
                             ['id', 'age', 'sex', "birthday", 'inday', 'outday', 'stayday', 'dept1', 'dept2',
@@ -178,7 +179,7 @@ def main():
                              'pu-private',
                              "CHECK", 'from'])
         files = getfile(path, ".csv", ["_门诊", "_住院"])
-        for file in files:
+        for file in tqdm(files):
             data = pd.concat([data, pd.read_csv(file)], sort=False)
         data.reset_index(drop=True, inplace=True)
         print("编码中")
@@ -191,7 +192,7 @@ def main():
             os.system("python code_icd.py")
             result = pd.read_csv("./cache.csv")
             os.system("rm ./cache.csv")
-            for file in files:
+            for file in tqdm(files):
                 data = pd.read_csv(file)
                 data = pd.merge(data, result[['疾病名称', 'match', 'icd']], how="left", left_on="disease",
                                 right_on="疾病名称")  # 还要将icd的也搬出来
@@ -213,7 +214,7 @@ def main():
                 data.to_csv(re.sub("\.csv", ".csv", file), date_format="%Y/%m/%d", index=False)
             print("REPORT:\n完整性测试:", end='')
             data = pd.DataFrame()
-            for file in files:
+            for file in tqdm(files):
                 data = pd.concat([data, pd.read_csv(re.sub("\.csv", ".csv", file))], sort=False)
             print("MATCHED:{2}\nABSOLUTE_ACCURACY:{0}\nNOT_MATCHED:{1}\nBLANK:{3}" \
                   # .format(round(len(data.query("诊断名称==match")) / len(data), 2), \
@@ -234,6 +235,7 @@ def main():
     os.system("start {}".format(path))
     os.system("start {}\\log.txt".format(cwd))
     sys.exit(1)
+
 
 if __name__ == "__main__":
     main()
--- readmission.py
+++ readmission.py
@@ -1,48 +1,64 @@
+import pandas as pd, numpy as np;from numba import vectorize
+from tqdm import tqdm, tnrange, tqdm_notebook
+from it.data_clean import *;
+from multiprocessing import Pool
+from it import *;from datetime import datetime
+
+
+@vectorize(['float32(float32, float32)'], target='cuda')
+def minus(qua,qub):
+    return qua-qub
+
+def run(id):
+    qu = data.query('hp_name==@hp_name and id==@id').sort_values(['inday']).reset_index()
+    if len(qu) > 1:
+        qub = np.asarray(np.append(qu.inday, [np.nan]), dtype=np.float32)
+        qua = np.asarray(np.append([np.nan], qu.inday), dtype=np.float32)
+        data.loc[qu.index, 'read_time'] = np.abs(minus(qua,qub))[:len(qu)]
+        del qua, qub, qu
+    # for i in range(len(qu)):
+    #     try:
+    #          data.loc[qu['index'][i], 'read_time'] = abs(qu.inday[i] - qu.inday[i - 1])
+    #     except:
+    #         data.loc[qu['index'][i], 'read_time'] = np.nan
+
+def count_interval(hp_name):
+    return
+    # for id in tqdm(pd.Categorical(data.query('hp_name==@hp_name and read==True').id).categories):
+
+
 if __name__=='__main__':
-    import pandas as pd, numpy as np;
-    from tqdm import tqdm, tnrange, tqdm_notebook
-    from it.data_clean import *;
-    from multiprocessing import Process, Queue, Pool, Lock
-    from it import *;from datetime import datetime
-
-    def count_interval(hp_name):
-        print(hp.index(hp_name), "-", len(hp), ':', hp_name)
-        for id in tqdm(pd.Categorical(data.query('hp_name==@hp_name and read==True').id).categories):
-            qu = data.query('hp_name==@hp_name and id==@id').sort_values(['inday']).reset_index()
-            for i in range(len(qu)):
-                try:
-                    data.loc[qu['index'][i], 'read_time'] = abs(qu.inday[i] - qu.inday[i - 1])
-                except:
-                    data.loc[qu['index'][i], 'read_time'] = np.nan
-
-    try:data = pd.read_csv("/d/data/merge_coded.csv")
-    except: data = pd.read_csv("d://data/merge_coded.csv")
-    # data['inday']=data.inday.apply(standarizedate)
-    data.dropna(subset=['inday', 'age', 'id', 'hp_name', 'hc'])
-    data = data[data.id.notna()]
-    for var in ['birthday', 'inday', 'outday']:
-        data[var] = pd.to_datetime(data[var], errors='coerce', yearfirst=False, format="%Y/%m/%d")
-    data.inday2 = data.inday
-    data['inday'] = data.inday.apply(lambda x: pd.Timestamp.to_julian_date(x) if isinstance(x, datetime) else np.nan)
-    data['age'] = data.age.apply(age_cut)
-    data.sort_values(['inday'], inplace=True)
-    data['read'] = data.duplicated(['id', 'age', 'hp_name'])
-    data['read_time'] = 0
+    try:data = pd.read_csv("/D/data/read.csv")
+    except:
+        data = pd.read_csv("/D/data/merge_coded.csv")
+        # data['inday']=data.inday.apply(standarizedate)
+        data.dropna(subset=['inday', 'age', 'id', 'hp_name', 'hc'])
+        data = data[data.id.notna()]
+        for var in ['birthday', 'inday', 'outday']:
+            data[var] = pd.to_datetime(data[var], errors='coerce', yearfirst=False, format="%Y/%m/%d")
+        data['inday2'] = data.inday
+        data['inday'] = data.inday.apply(
+            lambda x: pd.Timestamp.to_julian_date(x) if isinstance(x, datetime) else np.nan)
+        data['age'] = data.age.apply(age_cut)
+        data.sort_values(['inday'], inplace=True)
+        data['read'] = data.duplicated(['id', 'age', 'hp_name'])
+        data['read_time'] = 0
+        data['process']=0
+        data = data[['inday', 'id', 'hp_name', "read", 'read_time','process']]
     hp = list(pd.Categorical(data.hp_name).categories)
-    with Pool(3) as pool:
-        pool.map(count_interval,hp)
-
-    # for hp_name in hp:
-    #     p = Count_interval(hp_name)
-    #     p.start()
-    #     ps.append(p)
-        # print(hp.index(hp_name), "-", len(hp), ':', hp_name)
-        # ps=[]
-        # for id in tqdm(pd.Categorical(data.query('hp_name==@hp_name and read==True').id).categories):
-        #     qu = data.query('hp_name==@hp_name and id==@id').sort_values(['inday']).reset_index()
-        #     for i in range(len(qu)):
-        #         try:
-        #             data.loc[qu['index'][i], 'read_time'] = abs(qu.inday[i] - qu.inday[i - 1])
-        #         except:
-        #             data.loc[qu['index'][i], 'read_time'] = np.nan
-    # for p in tqdm(ps): p.join()
+    # with Pool(2) as pool:
+    #     pool.map(count_interval,hp)
+    for hp_name in hp:
+        if any(data.query("hp_name==@hp_name and process==1").index):continue
+        print(hp.index(hp_name), "-", len(hp), ':', hp_name)
+        # for i in tqdm(list(pd.Categorical(data.query('hp_name==@hp_name and read==True').id).categories)):
+        #     run(i)
+        with Pool(5) as p:
+            max_ = len(data.query("hp_name==@hp_name"))
+            with tqdm(total=max_) as pbar:
+                for i, _ in enumerate(p.imap_unordered(
+                        run, list(pd.Categorical(data.query('hp_name==@hp_name and read==True').id).categories))):
+                    pbar.update()
+        data.loc[data.query("hp_name==@hp_name").index,'process']=1
+        data.to_csv("/D/data/read.csv",index=False,date_format="%Y%m%d")
+    data.to_csv("/D/data/merge_coded_read.csv",index=False,date_format="%Y%m%d")
